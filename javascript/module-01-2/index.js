53d303ff3473872789f1903f6d5e369d7ba73bc0
yuliame <adzumi1993@gmail.com>
2018-10-16 21:17:29 +0300

    code style improvement

--- javascript/module-01-2/index.js
+++ javascript/module-01-2/index.js
@@ -6,28 +6,25 @@ const amountOfSeats = prompt("Please, enter the amount of seats needed");
 
 if (amountOfSeats > 0 && Number.isInteger(+amountOfSeats))  {
     if (amountOfSeats <= taba) {
-       const isAgree =  confirm("Согласны ли Вы быть в группе Taba?");
-       if (isAgree === true) {
-        alert("Приятного путешествия в группе Taba");
+       if (confirm ("Согласны ли Вы быть в группе Taba?")) {
+            alert("Приятного путешествия в группе Taba");
        } else {
-        alert("Нам очень жаль, приходите еще!");
+            alert("Нам очень жаль, приходите еще!");
        }
     } else if (amountOfSeats > taba && amountOfSeats <= sharm) {
-        const isAgree =  confirm("Согласны ли Вы быть в группе Sharm");
-        if (isAgree === true) {
-        alert("Приятного путешествия в группе Sharm");
+        if (confirm ("Согласны ли Вы быть в группе Sharm?")) {
+            alert("Приятного путешествия в группе Sharm");
         } else {
-        alert("Нам очень жаль, приходите еще!");
+            alert("Нам очень жаль, приходите еще!");
         }
     } else if (amountOfSeats > sharm && amountOfSeats <= hurgada) {
-        const isAgree =  confirm("Согласны ли Вы быть в группе Hurgada");
-        if (isAgree === true) {
-        alert("Приятного путешествия в группе Hurgada");
+        if (confirm("Согласны ли Вы быть в группе Hurgada")) {
+            alert("Приятного путешествия в группе Hurgada");
         } else {
-        alert("Нам очень жаль, приходите еще!");
+            alert("Нам очень жаль, приходите еще!");
         }
     } else if (amountOfSeats > hurgada) {
-        alert("Извините, столько мест нет ни в одной группе!");
+            alert("Извините, столько мест нет ни в одной группе!");
     } 
 
 } else {
